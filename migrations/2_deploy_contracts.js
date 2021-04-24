714b4ada5b1b19df555c094fbaf40882c064cc5f
dankey123 <80100051+dankey123@users.noreply.github.com>
2021-04-24 21:00:06 +0530

    Initial commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,16 @@
+
+var SafeFork = artifacts.require("SafeFork");
+
+module.exports = function(deployer, network, accounts) {
+
+const name = "SafeFork"
+const symbol = "SF"
+const decimals = 18
+const amount = 1000000
+const tokens = web3.utils.toWei(amount.toString(), 'ether')
+
+deployer.then(function() {
+  return deployer.deploy(SafeFork, name, symbol, decimals, tokens).then(function() {
+    });
+  });
+};
